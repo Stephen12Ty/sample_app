f8cccc92e7cf2d988ecca755126943abc6a211e3
Stephen12Ty <stndrwod@memphis.edu>
2014-09-22 20:03:40 -0500

    Finish user signup finally

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user=User.new(params[:user])
-  	if @user.save
-      flash[:success] = "Welcome to the Sample App!"  
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-  	else
-  		render 'new'
-  	end
-
-
-
+    else
+      render 'new'
+    end
   end
   
   private
 
-      def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,6 +11,14 @@ describe "User Pages" do
     it {should have_title(full_title('Sign up'))}
   end
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user)}
+    before {visit user_path(user)}
+
+    it {should have_content(user.name)}
+    it {should have_title(user.name)}
+  end
+
   describe "signup" do
     before { visit signup_path}
     let(:submit){"Create my account"}
@@ -35,12 +43,6 @@ describe "User Pages" do
     end
   end
   
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user)}
-  	before {visit user_path(user)}
-
-  	it {should have_content(user.name)}
-  	it {should have_title(user.name)}
-  end
+  
 end
 
